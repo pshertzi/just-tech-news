deab060de8b4df2e2271b853143caf4da7c1aef5
Paul Shertzinger <pshertzi@gmail.com>
2022-03-09 14:13:36 -0500

    vote function

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require("../../config/connection");
 
 //get all users
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -26,7 +33,13 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
     include: [
       {
         model: User,
@@ -58,15 +71,46 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
 //update post title
 
-router.put('/:id', (req, res) => {
-    Post.update(
-        {
-            title: req.body.title
-        },
-        {
+//upvote
+router.put('/upvote', (req, res) => {
+    //custom static method created in modle/post.js
+    Post.upvote(req.body, {Vote})
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+    router.put('/:id', (req, res) => {
+Post.update(
+    {
+    title: req.body.title
+    },
+    {
+    where: {
+        id: req.params.id
+    }
+    }
+)
+    .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+        return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
       where: {
         id: req.params.id
       }
@@ -83,24 +127,5 @@ router.put('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
-  //delete post
-  router.delete('/:id', (req, res) => {
-    Post.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-module.exports = router;
+  
+  module.exports = router;
