7d02c82dfc78c83f00417c334414b2556754b569
Paul Shertzinger <pshertzi@gmail.com>
2022-03-14 19:12:29 -0400

    login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,7 +52,16 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -76,9 +85,15 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
     res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
+});
 });
 
 
@@ -122,6 +137,17 @@ router.delete('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
